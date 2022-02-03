28000f50a2e2377bdd929d2181f5e5696522bdd9
Al <alain.yea.59@gmail.com>
2022-02-03 15:42:39 +0100

    base_1er_structure

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <link rel="icon" type="picture/png" href="pictures/favicon.png" />
+        <title>Accueil</title>
+    </head>
+
+    <!-- Debut body -->
+    <body>
+      <header>
+        <h1>Hello</h1>
+        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+      </header>
+
+      <nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">CV</a></li>
+        </ul>
+      </nav>
+
+      <section>
+         <aside>
+             <h1>Aside H1</h1>
+             <p>P Aside</p>
+        </aside>
+
+        <article>
+          <h1>Article H1</h1>
+          <p>P Article</p>
+        </article>
+      </section>
+
+
+        <footer>
+            <p>2021</p>
+        </footer>
+
+    </body>
+    <!-- Fin body -->
+</html>
